0f59f3bd9a2e3c879998def3db1973064351bfe3
pratik <pratikgtam@gmail.com>
2024-08-04 15:01:29 -0400

    image added

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pratik_portfolio/generated/assets.gen.dart';
 import 'package:pratik_portfolio/navigation/app_router.dart';
 import 'package:pratik_portfolio/shared/app_button.dart';
 import 'package:pratik_portfolio/shared/app_navigation_rail.dart';
@@ -12,43 +13,60 @@ class HomePage extends StatelessWidget {
     return SizedBox(
       height: MediaQuery.of(context).size.height - kBottomNavigationBarHeight,
       child: Responsiveness(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Row(
           children: [
-            Text(
-              'HEY THERE!',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const SizedBox(height: 20),
-            RichText(
-              text: TextSpan(
-                text: 'We are ',
-                style: Theme.of(context).textTheme.headlineMedium,
+            Expanded(
+              flex: 5,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  TextSpan(
-                    text: 'Gorkhali Developers'.toUpperCase(),
-                    style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                          color: Theme.of(context).colorScheme.tertiary,
+                  Text(
+                    'HEY THERE!',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  const SizedBox(height: 20),
+                  RichText(
+                    text: TextSpan(
+                      text: 'We are ',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                      children: [
+                        TextSpan(
+                          text: 'Gorkhali Developers'.toUpperCase(),
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineLarge
+                              ?.copyWith(
+                                color: Theme.of(context).colorScheme.tertiary,
+                              ),
                         ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    '''If you are seeking a top-rated freelancer with a proven track record of success, look no further. Let's embark on a journey together, where your vision becomes a reality. Get in touch with me today, and let's bring your projects to new heights!''',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+
+                  // This is the button that will navigate to the contact us page
+                  const SizedBox(height: 20),
+                  AppButton(
+                    text: 'Get started now',
+                    onPressed: () {
+                      // This will navigate to the contact us page
+                      const ContactMeRoute().go(context);
+                    },
                   ),
                 ],
               ),
             ),
-            const SizedBox(height: 20),
-            Text(
-              '''If you are seeking a top-rated freelancer with a proven track record of success, look no further. Let's embark on a journey together, where your vision becomes a reality. Get in touch with me today, and let's bring your projects to new heights!''',
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-
-            // This is the button that will navigate to the contact us page
-            const SizedBox(height: 20),
-            AppButton(
-              text: 'Get started now',
-              onPressed: () {
-                // This will navigate to the contact us page
-                const ContactMeRoute().go(context);
-              },
+            Expanded(
+              flex: 2,
+              child: Image.asset(
+                Assets.homePageImage.path,
+                fit: BoxFit.cover,
+              ),
             ),
           ],
         ),
